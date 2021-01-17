72c64e301b864f4c65e8b0fde700335b5da6ef07
Aakash Sigdel <aakash.sigdel@gmail.com>
2021-01-17 12:14:49 +0100

    Dispatch input events through adb

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const keysMap: { [k: string]: string } = {
+  backspace: '4',
+  up: '19',
+  down: '20',
+  left: '21',
+  right: '22',
+  return: '23',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { Command, flags } from '@oclif/command'
+import * as readline from 'readline'
+import { exec } from 'child_process'
+
+import { keysMap } from './constants'
 
 class AndroidShellInput extends Command {
   static description = ''
@@ -13,7 +17,36 @@ class AndroidShellInput extends Command {
     }),
   }
 
+  listen() {
+    readline.emitKeypressEvents(process.stdin)
+    process.stdin?.setRawMode?.(true)
+
+    process.stdin.on('keypress', (_, key: readline.Key) => {
+      let keySequence: string = ''
+      if (key.shift || key.ctrl) {
+        keySequence = key.sequence ?? ''
+      } else {
+        keySequence = key.name ?? ''
+      }
+
+      if (keySequence === '\u0003') {
+        process.exit()
+      }
+
+      this.dispatch(keySequence)
+    })
+  }
+
+  dispatch(keySequence: string) {
+    if (keySequence in keysMap) {
+      exec(`adb shell input keyevent ${keysMap[keySequence]}`)
+    } else {
+      exec(`adb shell input text ${keySequence}`)
+    }
+  }
+
   async run() {
+    this.listen()
     // const { args, flags } = this.parse(AndroidShellInput)
     //
     // const name = flags.name ?? 'world'
