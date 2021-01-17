7003e5cf49f8b959ad62ef6bde5fda63c65ccb40
Aakash Sigdel <aakash.sigdel@gmail.com>
2021-01-17 13:06:58 +0100

    Read from stored config and merge

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Command, flags } from '@oclif/command'
 import * as readline from 'readline'
 import { exec } from 'child_process'
+import { existsSync, lstatSync, promises } from 'fs'
 
 import { keysMap } from './constants'
-import { processKeySequence } from './utils'
+import { processKeySequence, mergeConfigs, KeyConfig } from './utils'
 
 class AndroidShellInput extends Command {
   static description = ''
@@ -14,11 +15,10 @@ class AndroidShellInput extends Command {
     config: flags.string({
       char: 'c',
       description: 'path to the config file',
-      default: './config',
     }),
   }
 
-  listen() {
+  listen(keyConfig: KeyConfig) {
     readline.emitKeypressEvents(process.stdin)
     process.stdin?.setRawMode?.(true)
 
@@ -29,27 +29,41 @@ class AndroidShellInput extends Command {
         process.exit()
       }
 
-      this.dispatch(keySequence)
+      this.dispatch(keyConfig, keySequence)
     })
   }
 
-  dispatch(keySequence: string) {
-    if (keySequence in keysMap) {
-      exec(`adb shell input keyevent ${keysMap[keySequence]}`)
+  dispatch(keyConfig: KeyConfig, keySequence: string) {
+    if (keySequence in keyConfig) {
+      exec(`adb shell input keyevent ${keyConfig[keySequence]}`)
     } else {
       exec(`adb shell input text ${keySequence}`)
     }
   }
 
+  async readFile(path: string): Promise<{ [key: string]: string }> {
+    const data = await promises.readFile(path)
+    return JSON.parse(data.toString())
+  }
+
+  async processConfig(configPath: string) {
+    const { isFile } = await promises.lstat(configPath)
+    if (isFile()) {
+      const config = await this.readFile(configPath)
+      const finalConfig = mergeConfigs(keysMap, config)
+      this.listen(finalConfig)
+    } else {
+      throw new Error('Invalid config file')
+    }
+  }
+
   async run() {
-    this.listen()
-    // const { args, flags } = this.parse(AndroidShellInput)
-    //
-    // const name = flags.name ?? 'world'
-    // this.log(`hello ${name} from ./src/index.ts`)
-    // if (args.file && flags.force) {
-    //   this.log(`you input --force and --file: ${args.file}`)
-    // }
+    const { flags } = this.parse(AndroidShellInput)
+    if (flags.config) {
+      this.processConfig(flags.config)
+    } else {
+      this.listen(keysMap)
+    }
   }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,19 @@
 import { Key } from 'readline'
 
+export interface KeyConfig {
+  [key: string]: string
+}
+
 export function processKeySequence(key: Key): string {
   if (key.shift || key.ctrl || !key.name) {
     return key.sequence ?? ''
   }
   return key.name ?? ''
+}
+
+export function mergeConfigs(
+  config1: KeyConfig,
+  config2: KeyConfig
+): KeyConfig {
+  return { ...config1, ...config2 }
 }
