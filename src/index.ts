ab3ba66dcfd9e102866efdc0a6d6c47ce659eb16
Aakash Sigdel <aakash.sigdel@gmail.com>
2021-01-17 09:13:04 +0100

    refactor flags

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
 import { Command, flags } from '@oclif/command'
 
 class AndroidShellInput extends Command {
-  static description = 'describe the command here'
+  static description = ''
 
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
+    config: flags.string({
+      char: 'c',
+      description: 'path to the config file',
+      default: './config',
+    }),
   }
 
-  static args = [{ name: 'file' }]
-
   async run() {
-    const { args, flags } = this.parse(AndroidShellInput)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    // const { args, flags } = this.parse(AndroidShellInput)
+    //
+    // const name = flags.name ?? 'world'
+    // this.log(`hello ${name} from ./src/index.ts`)
+    // if (args.file && flags.force) {
+    //   this.log(`you input --force and --file: ${args.file}`)
+    // }
   }
 }
 
