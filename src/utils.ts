ddb578e93b645e38d7e6a8a0433c7b782611ed4a
Aakash Sigdel <aakash.sigdel@gmail.com>
2021-01-24 09:47:43 +0100

    Add utils to get and require platfroms

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,20 @@ export interface KeyConfig {
   [key: string]: string
 }
 
+export enum Platform {
+  AMAZONFIRE = 'amazonfire',
+  DEFAULT = 'default',
+}
+
+export function getAvailablePlatforms(): Platform[] {
+  return Object.values(Platform)
+}
+export function getAvailablePlatformsString(): string {
+  return getAvailablePlatforms()
+    .filter((p) => p !== 'default')
+    .join(', ')
+}
+
 export function processKeySequence(key: Key): string {
   if (key.shift || key.ctrl || !key.name) {
     return key.sequence ?? ''
@@ -16,4 +30,17 @@ export function mergeConfigs(
   config2: KeyConfig
 ): KeyConfig {
   return { ...config1, ...config2 }
+}
+
+export function requireConfig(platform: Platform): KeyConfig {
+  const defaultConfig: KeyConfig = require(`./platformConfigs/${Platform.DEFAULT}`)
+
+  if (!(platform in Platform)) {
+    defaultConfig
+  }
+
+  return {
+    ...defaultConfig,
+    ...require(`./platformConfigs/${platform}`),
+  } as KeyConfig
 }
